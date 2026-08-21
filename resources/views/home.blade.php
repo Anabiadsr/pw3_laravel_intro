7c69044f67cfc2091bc337a92bab96560e4ec017
xuxadaalmeida <isaquealmeida412@gmail.com>
2026-08-21 17:23:24 -0300

    feature: admin

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title','inicio')
+
+@section('content')
+    <section>
+        <h2>Bem-Vindo ao projeto</h2>
+        <p>Esta tela reutiliza o layout padrão com header a footer.</p>
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,13 @@ use App\Http\Controllers\LivroController;
 use App\Models\Livro;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::view('/admin', 'admin.dashboard');
+Route::view('/landing', 'landing');
+
 Route::get('/livros', [LivroController::class, 'index']);
 
 Route::post('/livros', [LivroController::class, 'store']);
